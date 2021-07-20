22207f5446f14dc239034388d3df054e8e3b38e4
Paul <paul70079@gmail.com>
2021-07-20 19:44:36 +0200

    slight improvements

--- root/defaults/backup.sh
+++ root/defaults/backup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/with-contenv bash
 
-# restic -r <destination> --verbose backup /data --password-file=/config/password.txt
-# restic -r <destination> --verbose forget --keep-weekly 3 --keep-monthly 3 --keep-yearly 3 --password-file=/config/password.txt --prune
+# restic -r <destination> --verbose backup /data --password-file=/config/password.txt --tag <tag>
+# restic -r <destination> --verbose forget --keep-weekly 3 --keep-monthly 3 --keep-yearly 3 --password-file=/config/password.txt --prune --tag <tag>
